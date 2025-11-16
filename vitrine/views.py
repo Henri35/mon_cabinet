adb1695361c781b0dc136f58a79bab57c984df3a
Henri35 <dufour.fabien@outlook.fr>
2025-11-16 15:07:40 +0100

    Initial Django version

---
+++ vitrine/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "vitrine/index.html")
+
+def politique(request):
+    return render(request, 'vitrine/politique.html')
