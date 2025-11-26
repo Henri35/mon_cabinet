7325e88b65e93ca31739e75a377fc6de6bf05c3c
Henri35 <dufour.fabien@outlook.fr>
2025-11-26 16:50:26 +0100

    modif2

--- monsite/urls.py
+++ monsite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls), #fabien, Htbs171717!
     path('', include('vitrine.urls')),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
 ]
+
